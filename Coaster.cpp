c4ba24e7a672e8fc3cbf2f4aeebf54a74a61c87a
Per Ivar <Per Ivar@.(none)>
2011-04-07 22:02:31 +0200

    Faar ikke kjort programmet jeg naa

--- Coaster.cpp
+++ Coaster.cpp
@@ -198,10 +198,10 @@ void Coaster::createScene(void)
 	// Setup sound
 	//Ogre::Vector3 middle(track.getPos(0.5).x, track.getPos(0.5).y, track.getPos(0.5).z);
 	Ogre::Vector3 middle(0,0,0);
-	testNode = this->mSceneMgr->getRootSceneNode()->createChildSceneNode("Testnode", middle);
+	//testNode = this->mSceneMgr->getRootSceneNode()->createChildSceneNode("Testnode", middle);
 				
-	soundEngine = new SoundEngine(physicsCart, cartNode);
-	soundEngine->playBackgroundSounds(true);
+	//soundEngine = new SoundEngine(physicsCart, cartNode);
+	//soundEngine->playBackgroundSounds(true);
 	
 	//soundEngine->addSound(SoundEngine::BLIZZARD01, cartNode);
 
@@ -285,9 +285,9 @@ bool Coaster::frameRenderingQueued(const Ogre::FrameEvent& arg)
 
 		// Update sound engine
 		if (cameraName == "CartCam")
-			soundEngine->frameStarted(cartNode, dt);
+			;//soundEngine->frameStarted(cartNode, dt);
 		else if (cameraName == "PlayerCam") {
-			soundEngine->frameStarted(testNode, dt);	// TODO: insert a scene node for the 3rd person camera here
+			;//soundEngine->frameStarted(testNode, dt);	// TODO: insert a scene node for the 3rd person camera here
 		}
 	}
 	
